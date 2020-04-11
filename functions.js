ea6b48d2a1928f509287acf42fd307f52e017a27
unknown <jacovancr@gmail.com>
2020-04-11 17:16:19 +0100

    Basic boundary logic for farming

--- functions.js
+++ functions.js
@@ -44,14 +44,12 @@ function get_best_attack() {
 }
 
 var char_range = character.range * 1; //character.range;
+
 function kite(target) { // TODO: Move this logic out of main loop (4 times a second) to a faster tick loop? May cause problems.
     var _dx = character.x - target.x;
     var _dy = character.y - target.y;
     var dx = abs(_dx); //The x difference
     var dy = abs(_dy); // The y difference
-
-    var distance = get_distance_between(character, target);
-
     var x = character.x;
     var y = character.y;
     var step_size = 11; // 11 seems to work best for "out of combat" walking to enemy without having a stutter walk. But perhaps this should not use the kite algorithm anyway
@@ -59,14 +57,14 @@ function kite(target) { // TODO: Move this logic out of main loop (4 times a sec
     if(dx < dy) { // Determine y and x movement absolute values (normalised to 1 for the larger value), multiplied by the defined 'step_size' value
         var ymove = (dy / dy) * step_size; // (dy/dy) = 1 (but keeps pos/negative sign)
         var xmove = (dx / dy) * step_size;
-       
     } else {
         var xmove = (dx / dx) * step_size; // (dx/dx) = 1 (but keeps pos/negative sign)
         var ymove = (dy / dx) * step_size;
     }
-
+    log(character.x + " " + character.y);
+    var distance = get_distance_between(character, target);
     if(distance < char_range) {
-        log("Get further away");
+        // log("Kiting: Get further away");
         // Get further away
         if(_dx > 0) {
             x = character.x + xmove; // Move right
@@ -79,7 +77,7 @@ function kite(target) { // TODO: Move this logic out of main loop (4 times a sec
             y = character.y - ymove; // Move down
         }
     } else if(distance > char_range + (step_size)) { // If out of range (by more than 1 step sizes)
-        log("Get closer");
+        // log("Kiting: Get closer");
         // Get closer
         if(_dx > 0) {
             x = character.x - xmove; // Move left
@@ -92,16 +90,14 @@ function kite(target) { // TODO: Move this logic out of main loop (4 times a sec
             y = character.y + ymove; // Move up
         }
     } else { // If only just out of range, don't move just wait
-        // Do nothing - mob walk towards you quicker than worth taking a step.
-
-        //TODO: this should trigger getting closer still, if the user has not started combat with the target yet (to prevent standing there like an idiot while the mob is walking parallel to the char)
+        // log("Kiting: Don't move just wait");
     }
     move(
         x,
         y
     )
-}
 
+}
 function get_distance_between(p1, p2) {
     var dx = abs(p1.x - p2.x); //The x difference
     var dy = abs(p1.y - p2.y); // The y difference
@@ -111,4 +107,29 @@ function get_distance_between(p1, p2) {
     var D = Math.sqrt(d2); // The distance between char and target
 
     return D;
+}
+
+/* Movement automation functions */
+function stay_in_bounds() {
+    if(is_out_of_bounds()) {
+        log("hit");
+		attack_mode = false;
+		move(reset.x, reset.y).then(function(){
+            log("hit2");
+		    // change_target(target);
+            attack_mode=true;
+        });
+	}
+}
+function is_out_of_bounds() {
+    var sides = reset.square_area / 2;
+    if(character.x > reset.x + sides 
+    || character.x < reset.x - sides 
+    || character.y > reset.y + sides
+    || character.y < reset.y - sides) {
+        log("out");
+        return true;
+    }
+    log("in");
+    return false;
 }
--- main.js
+++ main.js
@@ -1,14 +1,35 @@
-var attack_mode=false;
+var attack_mode = true;
+var kite_mode = true;
+var reset = { // Coordinates to reset to when out of bounds
+    x: 490,
+	y: 1080,
+	square_area: 500 // Stay within an area around x,y of size
+}
+
 setInterval(function(){
+	log("__________________________________________"); //Easier debugging 'per interval'
+    log("Character position: " + Math.round(character.x) + ", " + Math.round(character.y));
 	use_pots();
 	loot();
-	set_attack_mode();
-	
+	// set_attack_mode();
+
 	if(!attack_mode || character.rip || is_moving(character)) return;
 
+	// stay_in_bounds();
+	if(is_out_of_bounds() && attack_mode == true) {
+        log("hit");
+		attack_mode = false;
+		move(reset.x, reset.y).then(function(){
+            log("hit2");
+		    // change_target(target);
+			attack_mode=true;
+        });
+		return;
+	}
+
 	var target=get_targeted_monster();
-	if(!target)
-	{
+	if(!target) {
+		// TODO only select monstor that is not in combat already?
 		target=get_nearest_monster({min_xp:100,max_att:120});
 		if(target) change_target(target);
 		else
@@ -17,18 +38,15 @@ setInterval(function(){
 			return;
 		}
 	}
-	
-    if(can_attack(target))
-	{
+
+	if(can_attack(target)) {
         set_message("Attacking");
 		use_best_attack(target);
-    } else {
-        // Kite the target
-        // console.log("char: ", character.x + ", " + character.y);
-        // console.log("targ: ", target.x + ", " + target.y);
-        // console.log("____");
-        kite(target);
     }
+	// Kite the target
+	if(kite_mode) {
+		kite(target);
+	}
 
 },1000/4); // Loops every 1/4 seconds.
 
